7a2f36d4daa80c0e4de04ed647d56b3f2eaccb5a
eroo36 <edogruca@hotmail.com>
2023-04-18 17:55:12 +0300

    init

---
+++ dist/index.esm.js
@@ -0,0 +1,8 @@
+import * as preact from 'preact';
+import * as emotion from 'emotion';
+export * from 'emotion';
+import createEmotionStyled from 'create-emotion-styled';
+
+var index = createEmotionStyled(emotion, preact);
+
+export default index;
